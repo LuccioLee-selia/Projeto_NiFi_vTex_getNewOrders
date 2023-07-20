4a4dc7206cce5ae63fc83bfab9fba7081765e342
Luccio <lucio.lee@seliafullservice.com.br>
2023-07-20 14:51:12 -0300

    working getNewOrdeersVtex

--- sql_scripts.py
+++ sql_scripts.py
@@ -1,12 +1,32 @@
 
 queryList = {
-    'queryVtexAccounts': '''
-                        SELECT 
-                            CONTA, 
-                            CHAVE, 
-                            TOKEN 
-                        FROM 
-                            acessos_vtex 
-                        WHERE 
-                            ATIVO = 'T' '''
+    'getAllNewOrdersVtex_Recent' : {
+        'sql_query': '''
+                    SELECT 
+                        CONTA, 
+                        CHAVE, 
+                        TOKEN 
+                    FROM 
+                        acessos_vtex 
+                    WHERE 
+                        ATIVO = 'T' ''',
+        'nowStrftime' : "%Y-%m-%dT%H:%M:%S.%fZ",
+        'yesterdayStrftime' : "%Y-%m-%dT%H:%M:%S.%fZ",
+        'timedelta' : 1
+    },
+    'getAllNewOrdersVtex_30DaysOxitec': {
+        'sql_query': '''
+                    SELECT 
+                        CONTA,
+                        CHAVE,
+                        TOKEN 
+                    FROM 
+                        acessos_vtex
+                    WHERE 
+                        ACESSO_VTEX IN (22)''',
+        'nowStrftime' : "%Y-%m-%dT23:59:59.999Z",
+        'yesterdayStrftime' : "%Y-%m-%dT00:00:00.000Z",
+        'timedelta' : 30
+    }
+
 }
