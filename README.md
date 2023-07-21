b9924f864d81018498e26ba2f15ff8be2ad51e70
Luccio <lucio.lee@seliafullservice.com.br>
2023-07-21 14:34:59 -0300

    [FCT WIP] - GetOrderFromVtex
    [DOC] - Improved documentation for readme.md
    [UPD] - Updated getAllNewOrders to a better structure
    [PDN] - Pending vectorization (Optimization) for checking for list values in dataFrame before uploading to AzureDB

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Projeto_NiFi_vTex_getNewOrders
  projeto para substituir o getNewOrders/vTEX do NIFI
+
+
+
+
+
+
+
+[FCT] Description (New functionality added)
+[BUG.xx] Bug description (When solved) (For bugs – xx: Bug number in the issue tracker).
+[WIP] Description (Work in Progress – Lacks testing)
+[UPD] Updated to version 1.x.y (New Release – Can go to production)
+[DOC] Added doc (New Documentation)
+[QLTY] Description (Code refactoring and general beauty parameters)
+[OPT] Description (Optimizations – The code has been optimized)
+[PDN] Description (Feature pending)
--- sql_scripts.py
+++ sql_scripts.py
@@ -12,21 +12,58 @@ queryList = {
                         ATIVO = 'T' ''',
         'nowStrftime' : "%Y-%m-%dT%H:%M:%S.%fZ",
         'yesterdayStrftime' : "%Y-%m-%dT%H:%M:%S.%fZ",
-        'timedelta' : 1
+        'timedelta' : 1,
+        'resultSuccessTable' : 'temp_OrdersVtexSuccessRecent',
+        'resultFailedTable' : 'temp_OrdersVtexfailureRecent'
     },
-    'getAllNewOrdersVtex_30DaysOxitec': {
+    'getAllNewOrdersVtex_30Days': {
         'sql_query': '''
-                    SELECT 
+                    SELECT
                         CONTA,
                         CHAVE,
                         TOKEN 
                     FROM 
                         acessos_vtex
                     WHERE 
-                        ACESSO_VTEX IN (22)''',
+                        ATIVO = 'T'
+                    ''',
         'nowStrftime' : "%Y-%m-%dT23:59:59.999Z",
         'yesterdayStrftime' : "%Y-%m-%dT00:00:00.000Z",
-        'timedelta' : 30
+        'timedelta' : 30,
+        'resultSuccessTable' : 'temp_OrdersVtexSuccess30Days',
+        'resultFailedTable' : 'temp_OrdersVtexfailure30Days'
+    },
+    'getOrders_temptables' : {
+        'sql_query' : '''
+                    SELECT
+                        tovsr.orderId,
+                        av.CHAVE,
+                        av.TOKEN,
+                        tovsr.hostname as CONTA
+                    FROM
+                        [temp_OrdersVtexSuccessRecent] tovsr
+                    LEFT JOIN
+                        acessos_vtex av ON tovsr.hostname = av.CONTA
+                    WHERE
+                        tovsr.hostname <> 'distribuidor'
+                    ''',
+        'sql_querySequence' : '''
+                    SELECT
+                        NEXT VALUE FOR dbo.SEQ_ID_PEDIDO_VTEX as ID_PEDIDO
+                    '''
+    },
+    'getOrders_pedidosVtex' : {
+        'sql_query' : '''
+                    SELECT 
+                        pvtex.ID_PEDIDO_VTEX,
+                        pvtex.orderId, 
+                        avtex.CONTA,
+                        avtex.CHAVE, 
+                        avtex.TOKEN
+                    FROM pedidos_vtex pvtex
+                    INNER JOIN acessos_vtex avtex ON avtex.CONTA = pvtex.accountHostname
+                    WHERE status NOT IN ('invoiced','replaced','canceled')
+                    AND avtex.ATIVO = 'T'
+                    '''
     }
-
 }
